3301ad044b02e5f306ec0eb174c25b9485a38487
InnerDesires <vladyslavte@wix.com>
2025-08-11 00:46:14 +0300

    feat: update remote patterns

--- next.config.js
+++ next.config.js
@@ -6,11 +6,13 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
 
+const VERCEL_URL = `https://${process.env.VERCEL_URL}`
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[NEXT_PUBLIC_SERVER_URL, VERCEL_URL].map((item) => {
         const url = new URL(item)
 
         return {
